68d47a8ecaf9856d9f054ebcfd701f9701eec01a
piruto <wangkai@fuzhi.ai>
2024-01-08 13:22:17 +0800

    test

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+# -*- ecoding: utf-8 -*-
+# @ModuleName: streamlit_app
+# @Author: wk
+# @Email: 306178200@qq.com
+# @Time: 2024/1/8 13:21
+import streamlit as st
+
+st.title('我的第一个Streamlit应用')
+st.write('欢迎来到Streamlit的世界！')
